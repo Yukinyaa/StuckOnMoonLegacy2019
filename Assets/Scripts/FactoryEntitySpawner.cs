ee0c0abd0460dad010bcfbd37b5fe0d7ece7f96c
sharpencil改二 <yukinyaa712@gmail.com>
2019-01-21 20:31:26 +0900

    getting ecs ready

---
+++ Assets/Scripts/FactoryEntitySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Rendering;
+using Unity.Transforms;
+using Unity.Entities;
+
+public class FactoryEntitySpawner : MonoBehaviour
+{
+    private static EntityManager entityManager;
+    private static MeshRenderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public static void InitalizeWithScene()
+    {
+        renderer = FindObjectOfType<MeshRenderer>().Value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MapData/MapHandle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class MapHandle
+{
+    
+
+}
+
+public struct FactoryEntity
+{
+    Vector2 position;
+    bool isactive;
+}
